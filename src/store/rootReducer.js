c663ab7da102306b8edcf5c3f8f78bb1eebeb03e
Thinh D. Bui <jinsherlock@gmail.com>
2019-01-26 21:13:50 +0800

    Feat: add reduxform

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import tiles from './states/tiles';
+import sequence from './states/sequence';
+import resultList from './states/resultList';
 
 export const createReducer = () => combineReducers({
   machineId: (x = {}) => x,
+  form: formReducer,
   ...tiles,
+  ...sequence,
+  ...resultList,
 });
 
 export default createReducer;
