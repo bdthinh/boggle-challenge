ab7757dadfc35ec162c51b004bc1cfea7685349e
Thinh D. Bui <jinsherlock@gmail.com>
2019-01-26 21:14:02 +0800

    Feat: add state sequence

---
+++ src/store/states/sequence.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-loop-func */
+import { combineReducers } from 'redux';
+import { path } from 'lodash/fp';
+import { handleAction } from 'redux-actions';
+
+import { tilesPositionMapSelector } from './tiles';
+import { resultListSelector } from './resultList';
+
+import {
+  VALID_STATE,
+  checkCombinationInResultList,
+  checkCombinationPossible,
+} from './validations/sequenceValidation';
+
+export const UPDATE_CURRENT_SEQUENCE = 'SEQUENCE/UPDATE_CURRENT';
+export const RESET = 'SEQUENCE/RESET';
+
+export const currentSequenceSelector = path('sequence.current');
+
+export const updateSequence = currentSequence => ({
+  type: UPDATE_CURRENT_SEQUENCE,
+  payload: currentSequence || [],
+});
+
+export const resetSequence = () => ({ type: RESET });
+
+export const validateWord = combination => (dispatch, getState) => {
+  const state = getState();
+
+  const positionMap = tilesPositionMapSelector(state);
+  const resultList = resultListSelector(state);
+
+  const resultValidation = checkCombinationInResultList(combination, resultList);
+  if (!resultValidation.valid) {
+    return resultValidation;
+  }
+
+  const combinationValidation = checkCombinationPossible(combination, positionMap);
+  if (!combinationValidation.valid) {
+    dispatch(resetSequence());
+    return combinationValidation;
+  }
+  dispatch(updateSequence(combinationValidation.data));
+
+  return VALID_STATE;
+};
+
+const takePayload = (state, { payload }) => payload;
+
+const current = handleAction(
+  UPDATE_CURRENT_SEQUENCE,
+  takePayload,
+  [],
+);
+
+const sequence = (state, action) => {
+  if (action.type === RESET) {
+    // eslint-disable-next-line
+    state = undefined;
+  }
+  return combineReducers({
+    current,
+  })(state, action);
+};
+
+export default { sequence };
