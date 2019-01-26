66eec313d348d2330e72eb8fc97c9983359400f7
Thinh D. Bui <jinsherlock@gmail.com>
2019-01-26 18:42:25 +0800

    Feat: add reducer tiles

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+import tiles from './states/tiles';
+
 export const createReducer = () => combineReducers({
   machineId: (x = {}) => x,
+  ...tiles,
 });
 
 export default createReducer;
---
+++ src/store/states/tiles.js
@@ -0,0 +1,77 @@
+import { combineReducers } from 'redux';
+import { flow, over, path, reduce } from 'lodash/fp';
+import { handleAction } from 'redux-actions';
+
+import { getTiles } from '../mock/data';
+
+export const SET_TILES_INPUT = 'TILES/SET_TILES_INPUT';
+export const UPDATE_POSITION_MAP = 'TILES/UPDATE_POSITION_MAP';
+export const UPDATE_SEQUENCE = 'TILES/UPDATE_SEQUENCE';
+export const RESET = 'TILES/RESET';
+
+
+export const tilesInputSelector = path('tiles.input');
+export const tilesPositionMapSelector = path('tiles.positionMap');
+export const tilesCurrentSequenceSelector = path('tiles.currentSequence');
+
+const ALPHABETS = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+];
+
+export const setInput = tiles => ({ type: SET_TILES_INPUT, payload: tiles });
+
+export const updatePositionMap = positionMap => ({ type: UPDATE_POSITION_MAP, payload: positionMap });
+
+const reduceWithIndex = reduce.convert({ cap: false });
+
+const buildPositionMap = reduceWithIndex((book, value, index) => {
+  if (value !== '*') {
+    // eslint-disable-next-line
+    book[value] = book[value] || [];
+    book[value].push(index);
+  } else {
+    ALPHABETS.forEach(alphabet => {
+      // eslint-disable-next-line
+      book[alphabet] = book[alphabet] || [];
+      book[alphabet].push(index);
+    });
+  }
+  return book;
+}, {});
+
+
+export const setupTilesBoard = () => dispatch => (
+  getTiles()
+    .then(flow(
+      over([setInput, buildPositionMap]),
+      dispatch
+    ))
+);
+
+const takePayload = (state, { payload }) => payload;
+
+const input = handleAction(
+  SET_TILES_INPUT,
+  takePayload,
+  [],
+);
+
+const positionMap = handleAction(
+  SET_TILES_INPUT,
+  takePayload,
+  {},
+);
+
+const tiles = (state, action) => {
+  if (action.type === RESET) {
+    // eslint-disable-next-line
+    state = undefined;
+  }
+  return combineReducers({
+    input,
+    positionMap,
+  })(state, action);
+};
+
+export default { tiles };
