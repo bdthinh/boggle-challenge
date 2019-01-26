4a2bd1c30805e0f083b13ffe091794f5d9d34b68
Thinh D. Bui <jinsherlock@gmail.com>
2019-01-26 21:14:08 +0800

    Feat: add state resultList

---
+++ src/store/states/resultList.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-loop-func */
+import { path } from 'lodash/fp';
+import { handleAction } from 'redux-actions';
+
+export const APPEND_WORD = 'RESULT/APPEND_WORD';
+
+export const resultListSelector = path('resultList');
+
+export const appendWord = word => ({ type: APPEND_WORD, payload: word });
+
+const resultList = handleAction(
+  APPEND_WORD,
+  (state, { payload }) => ([...state, payload]),
+  [],
+);
+
+export default { resultList };
