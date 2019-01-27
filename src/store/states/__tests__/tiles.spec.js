0654e2e755f7ec75913346bfbd256253229191a1
Thinh D. Bui <jinsherlock@gmail.com>
2019-01-27 14:13:38 +0800

    Chore: update tiles spec

--- src/store/states/__tests__/tiles.spec.js
+++ src/store/states/__tests__/tiles.spec.js
@@ -8,8 +8,10 @@ import {
   buildPositionMap,
   setupTilesBoard,
 } from '../tiles';
-
-const EXAMPLE_TILES = ['T', 'A', 'P', '*', 'E', 'A', 'K', 'S', 'O', 'B', 'R', 'S', 'S', '*', 'X', 'D'];
+import {
+  EXAMPLE_TILES,
+  EXAMPLE_POSITION_MAP,
+} from './mockData';
 
 describe('tiles state', () => {
   const state = {
@@ -34,34 +36,7 @@ describe('tiles state', () => {
   });
 
   it('should build correct positionMap', () => {
-    expect(buildPositionMap(EXAMPLE_TILES)).toEqual({
-      T: [0, 3, 13],
-      A: [1, 3, 5, 13],
-      P: [2, 3, 13],
-      B: [3, 9, 13],
-      C: [3, 13],
-      D: [3, 13, 15],
-      E: [3, 4, 13],
-      F: [3, 13],
-      G: [3, 13],
-      H: [3, 13],
-      I: [3, 13],
-      J: [3, 13],
-      K: [3, 6, 13],
-      L: [3, 13],
-      M: [3, 13],
-      N: [3, 13],
-      O: [3, 8, 13],
-      Q: [3, 13],
-      R: [3, 10, 13],
-      S: [3, 7, 11, 12, 13],
-      U: [3, 13],
-      V: [3, 13],
-      W: [3, 13],
-      X: [3, 13, 14],
-      Y: [3, 13],
-      Z: [3, 13],
-    });
+    expect(buildPositionMap(EXAMPLE_TILES)).toEqual(EXAMPLE_POSITION_MAP);
   });
 
   it('should dispatch enough actions', async () => {
